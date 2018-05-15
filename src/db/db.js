0300b72272189de57b3557a1090b2fd35c2c0874
Celeste De Santiago <celestedaphne@gmail.com>
2018-05-15 10:54:53 -0700

    Configure DATABASE_URL for deployment

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')()
 
-const connectionString = 'postgres://localhost:5432/tarot_test'
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/tarot_test'
 /*  process.env.NODE_ENV === 'test'
     ? process.env.TEST_DB_URL
     : process.env.DB_URL */
